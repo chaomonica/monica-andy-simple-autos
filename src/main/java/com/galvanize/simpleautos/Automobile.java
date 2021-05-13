87e9790a35beaedd68983b4dd202ee78354178a5
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2021-05-13 09:35:25 -0700

    Mapped Automobile class to table in database

--- src/main/java/com/galvanize/simpleautos/Automobile.java
+++ src/main/java/com/galvanize/simpleautos/Automobile.java
@@ -1,11 +1,23 @@
 package com.galvanize.simpleautos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="automobiles")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Automobile {
-    
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
     private int year;
     private String make;
+    @Column(name="model_year")
     private String model;
     private String color;
+    @Column(name="owner_name")
     private String owner;
     private String vin;
 
@@ -70,5 +82,13 @@ public class Automobile {
 
     public void setVin(String vin) {
         this.vin = vin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
--- src/main/java/com/galvanize/simpleautos/AutosRepository.java
+++ src/main/java/com/galvanize/simpleautos/AutosRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+
 @Repository
 public interface AutosRepository extends JpaRepository <Automobile, Long>{
     List<Automobile> findByColor(String color);
